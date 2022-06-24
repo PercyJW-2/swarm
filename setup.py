c0aa757998500601deb28c5216db1c08c562638d
percyjw-2 <joris.wachsmuth@gmx.de>
2022-06-24 17:25:01 +0200

    Added Initial Project

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='swarm',
+    version='0.0.1',
+    description='',
+    url='https://gitlab.ub.uni-bielefeld.de/jwachsmuth/swarm',
+    author='Joris Wachsmuth',
+    author_email='jwachsmuth@techfak.de',
+    license='GNU LGPLv3',
+    packages=find_packages(
+        where='.',
+        include=['swarm*'],
+        exclude=['swarm.tests']
+    ),
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python :: 3.10'
+    ]
+)
---
+++ swarm/__init__.py
@@ -0,0 +1,6 @@
+"""
+Python library
+"""
+
+__version__ = "0.0.1"
+__author__ = 'Joris Wachsmuth'
